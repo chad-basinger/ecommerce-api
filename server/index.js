d5ef89b7b2aada0c4bc5277f5d32e0f6d4618f20
Chad Basinger <chad.basingeris@gmail.com>
2021-03-09 15:54:04 -0700

    lab ecommerce

---
+++ server/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const getProducts = require('./getProducts')
+const getProduct = require('./getProduct');
+const app = express();
+
+app.use(express.json());
+
+app.get(`/api/products`, getProducts)
+app.get(`api/products/:id`, getProduct)
+
+app.listen(4050, () => console.log('server is running on 4050'));
+
+
+
+
